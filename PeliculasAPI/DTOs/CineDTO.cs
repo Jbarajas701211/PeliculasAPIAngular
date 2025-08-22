38b270f544285955366bd3af9f4bfdaf608c4819
Jorge Barajas Estrada <jbarajas70@msn.com>
2025-08-22 13:46:55 -0600

    Revert "subir nuevamente"
    This reverts commit f43860f4072d261716ec5a86fa35a3f457775c04.

---
+++ PeliculasAPI/DTOs/CineDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeliculasAPI.DTOs
+{
+    public class CineDTO
+    {
+        [Required]
+        [StringLength(75)]
+        public required string Nombre { get; set; }
+        public required LocationDTO Ubicacion { get; set; }
+    }
+}
---
+++ PeliculasAPI/Utilidades/AutoMapperProfiles.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using NetTopologySuite.Geometries;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entidades;
+
+namespace PeliculasAPI.Utilidades
+{
+    public class AutoMapperProfiles: Profile
+    {
+        public AutoMapperProfiles()
+        {
+            ConfigurarMapeoGeneros();
+            ConfigurarMapeoActores();
+        }
+
+        private void ConfigurarMapeoGeneros()
+        {
+            CreateMap<GeneroCreacionDTO, Genero>();
+            CreateMap<Genero, GeneroDTO>();
+        }
+
+        private void ConfigurarMapeoActores()
+        {
+            CreateMap<ActorCreacionDTO, Actor>()
+                .ForMember(x => x.Foto, opciones => opciones.Ignore());
+            CreateMap<Actor, ActorDTO>();
+        }
+
+        private void ConfigurarCine()
+        {
+            CreateMap<Point, LocationDTO>()
+                .ForMember(dest => dest.Longitude, opt => opt.MapFrom(src => src.X))
+                .ForMember(dest => dest.Latitude, opt => opt.MapFrom(src => src.Y));
+
+            CreateMap<LocationDTO, Point>()
+                .ConstructUsing(src => new Point(src.Longitude, src.Latitude) { SRID = 4326 });
+
+            CreateMap<Cine, CineDTO>();
+
+            CreateMap<>
+        }
+    }
+}
